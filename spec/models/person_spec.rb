39771741a1e0d4f90090965d7669561ba3aca8eb
Ch4s3 <chase.gilliam@gmail.com>
2013-07-12 12:24:43 -0400

    nifty fix to the person spec

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Person do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let (:person) do
+    person = Person.new(first_name: 'Liz', last_name: 'White')
+  end
+
+  it 'is valid' do
+    expect(person).to be_valid
+  end
+
+  it 'is valid without a first name' do
+    person.first_name = nil
+    expect(person).not_to be_valid
+  end
+
+  it 'is valid without a last name' do
+    person.last_name = nil
+    expect(person).not_to be_valid
+  end
 end
