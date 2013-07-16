31d759c7d7b7efaba2899d946152585b2142564f
Ch4s3 <chase.gilliam@gmail.com>
2013-07-16 16:53:11 -0400

    company tests

--- spec/features/company_view_spec.rb
+++ spec/features/company_view_spec.rb
@@ -19,6 +19,39 @@ describe 'the company view', type: :feature do
         expect(page).to have_content(phone.number)
       end
     end
+
+    it 'has a link to add a new phone number' do
+      expect(page).to have_link('Add phone number', href: new_phone_number_path(contact_id: company.id, contact_type: "Company"))
+    end
+
+    it 'has links to edit phone numbers' do
+      company.phone_numbers.each do |phone|
+        expect(page).to have_link('edit', href: edit_phone_number_path(phone))
+      end
+    end
+
+    it 'edits a phone number' do
+      phone = company.phone_numbers.first
+      old_number = phone.number
+
+      first(:link, 'edit').click
+      page.fill_in('Number', with: '555-9999')
+      page.click_button('Update Phone number')
+      expect(current_path).to eq(company_path(company))
+      expect(page).to have_content('555-9999')
+      expect(page).to_not have_content(old_number)
+    end
+
+
+    it 'adds a new phone numer' do
+      page.click_link('Add phone number')
+      page.fill_in('Number', with: '555-8888')
+      page.click_button('Create Phone number')
+      expect(current_path).to eq(company_path(company.id))
+      expect(page).to have_content('555-8888')
+    end
+
+
   end
 
 end
