bb241aeb3cbd25c63b4a3d272d9cb49d46142ff7
Ch4s3 <chase.gilliam@gmail.com>
2013-07-16 10:10:49 -0400

    email table added and basic validation tests

---
+++ spec/models/email_address_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe EmailAddress do
+  let(:email_address) {EmailAddress.new(address: "jon@test.com", person_id: 1) }
+
+  it 'is valid' do
+    expect(email_address).to be_valid
+  end
+
+  it 'is invalid without and address' do
+    email_address.address = nil
+    expect(email_address).not_to be_valid
+  end
+
+  it 'is invalid without a person_id' do
+    email_address.person_id = nil
+    expect(email_address).not_to be_valid
+  end
+
+end
