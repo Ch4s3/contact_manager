cc2cdabc7cf58a6a9d170170559d735491aafed4
Ch4s3 <chase.gilliam@gmail.com>
2013-07-23 16:41:10 -0400

    delete bug

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,9 +1,6 @@
 class PhoneNumber < ActiveRecord::Base
 
-  before_validation(on: :create) do
-    self.number = number.gsub(/[^0-9]/, "") if attribute_present?("number")
-  end
-  before_validation(on: :update) do
+  before_validation do
     self.number = number.gsub(/[^0-9]/, "") if attribute_present?("number")
   end
 
