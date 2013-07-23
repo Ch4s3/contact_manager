217de4687d7ec62812181e20a0c13389d174a918
Ch4s3 <chase.gilliam@gmail.com>
2013-07-23 09:19:50 -0400

    contact list for people is done

--- spec/features/people_view_spec.rb
+++ spec/features/people_view_spec.rb
@@ -8,22 +8,23 @@ describe 'the people view', type: :feature do
     let(:user) { Fabricate(:user) }
 
     it 'displays people associated with the user' do
-      @person_1 = Fabricate(:person)
-      @person_1.user = user
-      @person_1.save
+      person_1 = Fabricate(:person)
+      person_1.user = user
+      person_1.save
       login_as(user)
       visit(people_path)
-      expect(page).to have_text(@person_1.to_s)
+      expect(page).to have_text(person_1.to_s)
     end
 
     it 'does not display people associated with other users' do
+      person_1 = Fabricate(:person)
       user_2 = User.create(name: "Bob", provider: "twitter", uid: "1234" )
       person_2 = Person.new(:first_name => 'Jon', :last_name => 'Jones')
       person_2.user_id = user_2.uid
       person_2.save
       login_as(user_2)
       visit(people_path)
-      expect(page).not_to have_text(@person_1.to_s)
+      expect(page).not_to have_text(person_1.to_s)
       #this ^ is written incorrectly as user_2 will be the same as user 1
     end
 
